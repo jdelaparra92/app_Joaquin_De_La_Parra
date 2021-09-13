bedb2817702a647078ed4dbddb17589b2dbcbb8b
Joaquin De La Parra <jdelaparra92@gmail.com>
2021-09-13 19:12:07 -0300

    Segunda entrega navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+const NavBar = () => {
+    return(
+    <div className="navContenedor">
+       <h3>Moodis E-Commerce</h3>
+       <nav>
+           <a href="#">Quienes Somos</a>
+           <a href="#">Productos</a>
+           <a href="#">Ofertas</a>
+           <a href="#">Contacto</a>
+       </nav>
+
+    </div>
+
+    )
+
+
+    
+}
+
+export default NavBar
